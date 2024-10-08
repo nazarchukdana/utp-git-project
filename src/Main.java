4f974f900c55ad65e40d5ef728b16d4b97e679b9
nazarchukdana <162808160+nazarchukdana@users.noreply.github.com>
2024-10-08 12:44:23 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
-        Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
-    }
+ public static void main(String[] args) {
+ Adder adder = new Adder();
+ System.out.println(adder.add(1, 2));
+ Subtractor subtractor = new Subtractor();
+ System.out.println(subtractor.subtract(6, 3));
+ }
 }
-
